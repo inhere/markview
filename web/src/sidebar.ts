c15737d73721d002991610006af32144cd0fb4dc
inhere <in.798@qq.com>
2026-04-02 17:35:25 +0800

    feat(files): 添加 debounce 工具函数

--- web/src/sidebar.ts
+++ web/src/sidebar.ts
@@ -12,6 +12,15 @@ import {
     readSidebarPreferences,
 } from './preferences';
 
+// Debounce 工具函数
+function debounce<T extends (...args: any[]) => any>(fn: T, delay: number): (...args: Parameters<T>) => void {
+    let timer: ReturnType<typeof setTimeout>;
+    return (...args: Parameters<T>) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => fn(...args), delay);
+    };
+}
+
 export interface FileTreeNode {
     name: string;
     href?: string;
